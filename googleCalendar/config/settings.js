2e20f5625df6f9301d1c202229b2c285bc357737
Johnervan <johnervan@gmail.com>
2018-01-26 16:59:47 +0800

    test

--- googleCalendar/config/settings.js
+++ googleCalendar/config/settings.js
@@ -1,6 +1,6 @@
 // const filepath = require('filepath');
 // var path = filepath.create("config", "key-file.pem");
-// var key_json = require("../TCC_Campus_Bot-39fcc3fba06a.json");
+var key_json = require("../TCC_Campus_Bot-39fcc3fba06a.json") || {};
 
 // Sample CalendarAPI settings
 const SERVICE_ACCT_ID = 'developer@tcc-campus-bot.iam.gserviceaccount.com';
