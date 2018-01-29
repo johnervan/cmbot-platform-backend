493a58e0b38855e449ce45f773134b71c6ddf0f9
Johnervan <johnervan@gmail.com>
2018-01-29 22:35:57 +0800

    shift security keys to heroku config var

--- googleCalendar/config/settings.js
+++ googleCalendar/config/settings.js
@@ -1,10 +1,6 @@
-// const filepath = require('filepath');
-// var path = filepath.create("config", "key-file.pem");
-var key_json = require("../TCC_Campus_Bot-39fcc3fba06a.json");
-
 // Sample CalendarAPI settings
 const SERVICE_ACCT_ID = 'developer@tcc-campus-bot.iam.gserviceaccount.com';
-const KEY = key_json.private_key;
+const KEY = process.env.GOOGLE_CAL_KEY.replace(/\\n/g, '\n');
 const TIMEZONE = 'UTC+08:00';
 const CALENDAR_ID = {
 	'primary': 't7cdso1jtsmtjqhlprn6om2h5g@group.calendar.google.com',
--- googleCalendar/events.js
+++ googleCalendar/events.js
@@ -5,9 +5,6 @@ const moment = require('moment');
 // Sample Execution
 // listSingleEventsWithinDateRange("2018-01-01T00:00:00+08:00","2018-12-01T00:00:00+08:00");
 
-const datetimeobj = moment("2018-03-03T20:00:00+08:00");
-console.log(datetimeobj.format("dddd, DD MMM YYYY"));
-
 function listSingleEventsWithinDateRange(startDateTime, endDateTime) {
   const cal = new CalendarAPI(config);
   const calendarIdList = config.calendarId;
