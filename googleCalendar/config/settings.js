7f92af30a30250887f8f93d62aaa2064281a1cc1
Johnervan <johnervan@gmail.com>
2018-02-04 09:57:48 +0800

    add tgbot_caller and remove google cal keys

--- googleCalendar/config/settings.js
+++ googleCalendar/config/settings.js
@@ -1,9 +1,9 @@
 // Sample CalendarAPI settings
-const SERVICE_ACCT_ID = 'developer@tcc-campus-bot.iam.gserviceaccount.com';
+const SERVICE_ACCT_ID = process.env.GOOGLE_CAL_ACCT;
 const KEY = process.env.GOOGLE_CAL_KEY.replace(/\\n/g, '\n');
 const TIMEZONE = 'UTC+08:00';
 const CALENDAR_ID = {
-	'primary': 't7cdso1jtsmtjqhlprn6om2h5g@group.calendar.google.com',
+	'primary': process.env.GOOGLE_CAL_ID,
 };
 
 module.exports.serviceAcctId = SERVICE_ACCT_ID;
